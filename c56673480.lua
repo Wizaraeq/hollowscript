01b7de81a2da7a339f5e35f431ea21305bc480ec
Wizaraeq <wizackyank@gmail.com>
2023-08-26 10:47:30 +0900

    fix

--- c56673480.lua
+++ c56673480.lua
@@ -50,8 +50,7 @@ function c56673480.initial_effect(c)
 	c:RegisterEffect(e7)
 end
 function c56673480.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLP(tp)>=1000 and Duel.IsPlayerCanDraw(tp,1)
-		and Duel.GetLP(1-tp)>=1000 and Duel.IsPlayerCanDraw(1-tp,1) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.IsPlayerCanDraw(1-tp,1) end
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,PLAYER_ALL,1)
 end
 function c56673480.activate(e,tp,eg,ep,ev,re,r,rp)
