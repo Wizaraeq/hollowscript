f6ac21bc8d58a50f56db0b915afa4fab57c687a3
Wizaraeq <wizackyank@gmail.com>
2023-11-19 14:07:29 +0900

    fix

--- c42952160.lua
+++ c42952160.lua
@@ -4,7 +4,6 @@ function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_GRAVE_SPSUMMON+CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_DAMAGE)
 	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
@@ -22,9 +21,20 @@ function s.filter(c,v,e,tp)
 	return c:IsAttackBelow(v) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return r&REASON_EFFECT>0 or Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,ev,e,tp) end
+	local is_eff=(r&REASON_EFFECT)==REASON_EFFECT
+	if chk==0 then return is_eff or (Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,ev,e,tp))
+	end
+	if is_eff then
+		e:SetCategory(CATEGORY_DAMAGE)
+		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ev*2)
+	else
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+	end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	local ss=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.filter),tp,LOCATION_GRAVE,0,1,nil,ev,e,tp)
 	local op=0
 	if r&(REASON_BATTLE+REASON_EFFECT)==REASON_BATTLE+REASON_EFFECT then
@@ -37,8 +47,18 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if op==1 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,ev,e,tp)
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		if Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_DAMAGE_STEP_END)
+			e1:SetOperation(s.skipop)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
+			Duel.RegisterEffect(e1,tp)
+		end
 	else
 		Duel.Damage(1-tp,ev*2,REASON_EFFECT)
 	end
+end
+function s.skipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SkipPhase(Duel.GetTurnPlayer(),PHASE_BATTLE,RESET_PHASE+PHASE_BATTLE_STEP,1)
 end
