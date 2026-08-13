ee932032ab76dee710825a1451da7abf5684a66b
Wizaraeq <wizackyank@gmail.com>
2026-08-13 22:02:57 +0900

    fix

--- c61822419.lua
+++ c61822419.lua
@@ -62,7 +62,7 @@ function s.nttg(e,c)
 	return c:IsLevelAbove(5)
 end
 function s.costchange(e,re,rp,val)
-	if re and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:GetHandler():IsType(TYPE_SPELL+TYPE_TRAP)
+	if re and (re:IsHasType(EFFECT_TYPE_ACTIVATE) or re:GetCode()==EFFECT_TRAP_ACT_IN_HAND or re:GetCode()==EFFECT_TRAP_ACT_IN_SET_TURN) and re:GetHandler():IsType(TYPE_SPELL+TYPE_TRAP)
 		and not Duel.IsChainSolving() then
 		return 0
 	else return val end
